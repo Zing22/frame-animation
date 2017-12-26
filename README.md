d6c4cb904028b76bc069ad412afb3eedf090e855
Zing22 <leezing22@gmail.com>
2017-12-26 21:27:23 +0800

    blog online!

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # frame-animation
 H5 canvas Frame animation library.
+
+H5序列帧动画播放库。
+
+
+## Dependency
+
+1. Jquery
+2. ion.sound
+
+
+## Live demo
+[Click here](http://jsbin.com/varijetagi)
+
+## 详细说明
+
+[CSDN Blog](http://blog.csdn.net/Mr_Zing/article/details/78900519)
--- demo.js
+++ demo.js
@@ -1,3 +1,31 @@
+function play_animation() {
+    var framesUrl = [];
+    for (let i = 1; i < 50; i++) {
+        framesUrl.push('http://ojcokbt6a.bkt.clouddn.com/frame-' + i + '.gif');
+    }
+
+    // frame animation
+    var ani = new frame_ani({
+        canvasTargetId: "c-1", // target canvas ID
+        framesUrl: framesUrl, // frames url
+        loop: true, // if loop
+        height: 264, // source image's height (px)
+        width: 263, // source image's width (px)
+        frequency: 20, // count of frames in one second
+        audioIonName: "bgm_1", // ion.sound audio name
+        onComplete: function () { // complete callback
+            console.log("Animation loop.");
+        },
+    });
+
+    // preload & play
+    ani.initialize(() => {
+        $("#loading").hide();
+        ani.play();
+    });
+}
+
+
 $(document).ready(() => {
     // ion.sound BGM
     ion.sound({
@@ -10,31 +38,12 @@ $(document).ready(() => {
         volume: 0.5,
         path: "http://ojcokbt6a.bkt.clouddn.com/", // my test URL
         preload: true,
-        multiplay: false
+        multiplay: false,
+        // 保证音频加载完成
+        ready_callback: () => {
+            play_animation();
+        }
     });
 
-    var framesUrl = [];
-    for (let i = 1; i < 50; i++) {
-        framesUrl.push('./images/frame-' + i + '.gif');
-    }
-
-    // frame animation
-    var ani = new frame_ani({
-        canvasTargetId: "c-1", // target canvas ID
-        framesUrl: framesUrl, // frames url
-        loop: true, // if loop
-        height: 264, // source image's height (px)
-        width: 263, // source image's width (px)
-        frequency: 20, // count of frames in one second
-        audioIonName: "bgm_1", // ion.sound audio name
-        onComplete: function() { // complete callback
-          console.log("Animation loop.");
-        },
-    });
-
-    // preload & play
-    ani.initialize(() => {
-        ani.play();
-    });
 
 });
